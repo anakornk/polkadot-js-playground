8c88d240d7fa3ab079cf3538187be528f2bcb7cc
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-07-09 15:32:06 +0800

    edit variable name

--- 06_make_a_transfer.js
+++ 06_make_a_transfer.js
@@ -15,10 +15,10 @@ async function main() {
   const alice = keyring.addFromUri("//Alice");
 
   // Create a extrinsic, transferring 12345 units to Bob
-  const transfer = api.tx.balances.transfer(BOB, 12345);
+  const extrinsic = api.tx.balances.transfer(BOB, 12345);
 
   // Sign and send the transaction using our account
-  const hash = await transfer.signAndSend(alice);
+  const hash = await extrinsic.signAndSend(alice);
 
   console.log("Transfer sent with hash", hash.toHex());
 }
